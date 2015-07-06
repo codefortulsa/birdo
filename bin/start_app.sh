7880c4a556ff4eb502eb7d125bed2156a59a49f7
Patrick Forringer <patrick@forringer.com>
2015-07-05 23:49:14 -0500

    remove preload from gunicorn command

--- bin/start_app.sh
+++ bin/start_app.sh
@@ -3,4 +3,4 @@
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-gunicorn birdo.wsgi:application -b 0.0.0.0:5000 --preload --log-level=debug --error-logfile -
+gunicorn birdo.wsgi:application -b 0.0.0.0:5000 --log-level=debug --error-logfile -
